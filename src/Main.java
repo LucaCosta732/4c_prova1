c8103f188db0d64a322fca746bcdb141dddd99e5
Costa <4CI@DESKTOP-4I0NS9T>
2022-04-27 11:46:07 +0200

    versione 1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @author Luca Costa
+ * @version 1.0
+ *
+ */
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Progetto di prova GitHub");
+		
+		
+	}
+
+}
